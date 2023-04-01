62f402c04acb5eb2e85a19a6b246b810eb1159aa
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-04-01 16:19:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://phoenix-monarch:token@github.com/phoenix-monarch/AUTO-FILTER-XYZ okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://phoenix-monarch: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@github.com/phoenix-monarch/AUTO-FILTER-XYZ okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
